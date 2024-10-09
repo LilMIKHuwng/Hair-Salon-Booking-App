88f145eb2962dba7c0f4637c53cdb281c5c143b7
KietBap <kietanh2622003@gmail.com>
2024-10-09 14:09:12 +0700

    Feat : fix api UserRole

--- HairSalonBookingApp/HairSalon.Contract.Services/Interface/IAppUserRoleService.cs
+++ HairSalonBookingApp/HairSalon.Contract.Services/Interface/IAppUserRoleService.cs
@@ -3,12 +3,11 @@ using HairSalon.ModelViews.AppUserRoleViewModels;
 
 namespace HairSalon.Contract.Services.Interface
 {
-	public interface IAppUserRoleService
-	{
-		Task<BasePaginatedList<AppUserRoleModelView>> GetAllAppUserRoleAsync(int pageNumber, int pageSize);
-		Task<AppUserRoleModelView> AddAppUserRoleAsync(CreateAppUserRoleModelView model);
-		Task<AppUserRoleModelView> UpdateAppUserRoleAsync(string UserId, string RoleId, UpdateAppUserRoleModelView model);
-		Task<string> DeleteAppUserRoleAsync(string UserId, string RoleId);
-		Task<AppUserRoleModelView> GetAppUserRoleAsync(string UserId, string RoleId);
-	}
+    public interface IAppUserRoleService
+    {
+        Task<BasePaginatedList<AppUserRoleModelView>> GetAllAppUserRoleAsync(int pageNumber, int pageSize, string? userId, string? roleId);
+        Task<string> AddAppUserRoleAsync(CreateAppUserRoleModelView model);
+        Task<string> UpdateAppUserRoleAsync(string UserId, string RoleId, UpdateAppUserRoleModelView model);
+        Task<string> DeleteAppUserRoleAsync(string UserId, string RoleId);
+    }
 }
--- HairSalonBookingApp/HairSalon.Services/Service/AppUserRoleService.cs
+++ HairSalonBookingApp/HairSalon.Services/Service/AppUserRoleService.cs
@@ -4,111 +4,165 @@ using HairSalon.Contract.Repositories.Interface;
 using HairSalon.Contract.Services.Interface;
 using HairSalon.Core;
 using HairSalon.ModelViews.AppUserRoleViewModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 
 namespace HairSalon.Services.Service
 {
-	public class AppUserRoleService : IAppUserRoleService
-	{
-		private readonly IUnitOfWork _unitOfWork;
-		private readonly IMapper _mapper;
+    public class AppUserRoleService : IAppUserRoleService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+        private readonly IHttpContextAccessor _contextAccessor;
 
-		public AppUserRoleService(IUnitOfWork unitOfWork, IMapper mapper)
-		{
-			_unitOfWork = unitOfWork;
-			_mapper = mapper;
-		}
+        public AppUserRoleService(IUnitOfWork unitOfWork, IMapper mapper, IHttpContextAccessor httpContextAccessor)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            _contextAccessor = httpContextAccessor;
+        }
 
-		public async Task<AppUserRoleModelView> AddAppUserRoleAsync(CreateAppUserRoleModelView model)
-		{
-			if (string.IsNullOrWhiteSpace(model.UserId))
-			{
-				throw new Exception("User ID cannot be empty.");
-			}
+        public async Task<string> AddAppUserRoleAsync(CreateAppUserRoleModelView model)
+        {
+            try
+            {
+                // check valid UserId
+                if (string.IsNullOrWhiteSpace(model.UserId))
+                {
+                    throw new Exception("User ID cannot be empty.");
+                }
 
-			ApplicationUserRoles newUserRole = _mapper.Map<ApplicationUserRoles>(model);
+                // Map data model to entity
+                ApplicationUserRoles newUserRole = _mapper.Map<ApplicationUserRoles>(model);
+                newUserRole.CreatedBy = _contextAccessor.HttpContext?.User?.FindFirst("userId")?.Value;
+                newUserRole.CreatedTime = DateTimeOffset.UtcNow;
+                newUserRole.LastUpdatedTime = DateTimeOffset.UtcNow;
 
-			newUserRole.CreatedBy = "claim account";
-			newUserRole.CreatedTime = DateTimeOffset.UtcNow;
-			newUserRole.LastUpdatedTime = DateTimeOffset.UtcNow;
+                // save changes
+                await _unitOfWork.GetRepository<ApplicationUserRoles>().InsertAsync(newUserRole);
+                await _unitOfWork.SaveAsync();
 
-			await _unitOfWork.GetRepository<ApplicationUserRoles>().InsertAsync(newUserRole);
-			await _unitOfWork.SaveAsync();
+                return "add new UserRole successfully";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error occurred while adding user role: {ex.Message}", ex);
+            }
+        }
 
-			return _mapper.Map<AppUserRoleModelView>(newUserRole);
-		}
+        public async Task<string> DeleteAppUserRoleAsync(string UserId, string RoleId)
+        {
+            try
+            {
+                // check valid UserId and RoleId
+                if (string.IsNullOrWhiteSpace(UserId) && string.IsNullOrWhiteSpace(RoleId))
+                {
+                    throw new Exception("Please provide a valid Application User Role ID.");
+                }
 
-		public async Task<string> DeleteAppUserRoleAsync(string UserId, string RoleId)
-		{
-			if (string.IsNullOrWhiteSpace(UserId) && string.IsNullOrWhiteSpace(RoleId))
-			{
-				throw new Exception("Please provide a valid Application User Role ID.");
-			}
+                // get UserRole by id and not deleted
+                ApplicationUserRoles existingUserRole = await _unitOfWork.GetRepository<ApplicationUserRoles>().Entities
+                    .FirstOrDefaultAsync(s => s.UserId == Guid.Parse(UserId) && s.RoleId == Guid.Parse(RoleId) && !s.DeletedTime.HasValue)
+                    ?? throw new Exception("The Application User Role cannot be found or has been deleted!");
 
-			ApplicationUserRoles existingUserRole = await _unitOfWork.GetRepository<ApplicationUserRoles>().Entities
-				.FirstOrDefaultAsync(s => s.UserId == Guid.Parse(UserId) && s.RoleId == Guid.Parse(RoleId) && !s.DeletedTime.HasValue)
-				?? throw new Exception("The Application User Role cannot be found or has been deleted!");
+                // set DeletedTime, DeletedBy
+                existingUserRole.DeletedTime = DateTimeOffset.UtcNow;
+                existingUserRole.DeletedBy = _contextAccessor.HttpContext?.User?.FindFirst("userId")?.Value;
 
-			existingUserRole.DeletedTime = DateTimeOffset.UtcNow;
-			existingUserRole.DeletedBy = "claim account";
+                // save changes
+                _unitOfWork.GetRepository<ApplicationUserRoles>().Update(existingUserRole);
+                await _unitOfWork.SaveAsync();
 
-			_unitOfWork.GetRepository<ApplicationUserRoles>().Update(existingUserRole);
-			await _unitOfWork.SaveAsync();
-			return "Deleted";
-		}
+                return "Deleted";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error occurred while deleting user role: {ex.Message}", ex);
+            }
+        }
 
-		public async Task<BasePaginatedList<AppUserRoleModelView>> GetAllAppUserRoleAsync(int pageNumber, int pageSize)
-		{
-			IQueryable<ApplicationUserRoles> roleQuery = _unitOfWork.GetRepository<ApplicationUserRoles>().Entities
-				.Where(p => !p.DeletedTime.HasValue)
-				.OrderByDescending(s => s.CreatedTime);
+        public async Task<BasePaginatedList<AppUserRoleModelView>> GetAllAppUserRoleAsync(int pageNumber, int pageSize, string? userId, string? roleId)
+        {
+            try
+            {
+                // get user role from database
+                IQueryable<ApplicationUserRoles> roleQuery = _unitOfWork.GetRepository<ApplicationUserRoles>().Entities
+                    .Where(p => !p.DeletedTime.HasValue)
+                    .OrderByDescending(s => s.CreatedTime);
 
-			int totalCount = await roleQuery.CountAsync();
+                // filter by userId
+                if (!string.IsNullOrEmpty(userId))
+                {
+                    roleQuery = roleQuery.Where(r => r.UserId.ToString().Equals(userId));
+                }
 
-			List<ApplicationUserRoles> paginatedShops = await roleQuery
-				.Skip((pageNumber - 1) * pageSize)
-				.Take(pageSize)
-				.ToListAsync();
+                // filter by roleId
+                if (!string.IsNullOrEmpty(roleId))
+                {
+                    roleQuery = roleQuery.Where(r => r.RoleId.ToString().Equals(roleId));
+                }
 
-			List<AppUserRoleModelView> appUserModelViews = _mapper.Map<List<AppUserRoleModelView>>(paginatedShops);
+                // Apply pagination
+                int totalCount = await roleQuery.CountAsync();
 
-			return new BasePaginatedList<AppUserRoleModelView>(appUserModelViews, totalCount, pageNumber, pageSize);
-		}
+                List<ApplicationUserRoles> paginatedShops = await roleQuery
+                    .Skip((pageNumber - 1) * pageSize)
+                    .Take(pageSize)
+                    .ToListAsync();
 
-		public async Task<AppUserRoleModelView> GetAppUserRoleAsync(string UserId, string RoleId)
-		{
-			if (string.IsNullOrWhiteSpace(UserId) && string.IsNullOrWhiteSpace(RoleId))
-			{
-				throw new Exception("Please provide a valid Application User Role ID.");
-			}
+                // map data to model
+                List<AppUserRoleModelView> appUserModelViews = _mapper.Map<List<AppUserRoleModelView>>(paginatedShops);
 
-			ApplicationUserRoles existingUserRole = await _unitOfWork.GetRepository<ApplicationUserRoles>().Entities
-				.FirstOrDefaultAsync(s => s.UserId == Guid.Parse(UserId) && s.RoleId == Guid.Parse(RoleId) && !s.DeletedTime.HasValue)
-				?? throw new Exception("The Application User Role cannot be found or has been deleted!");
+                return new BasePaginatedList<AppUserRoleModelView>(appUserModelViews, totalCount, pageNumber, pageSize);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error occurred while retrieving user roles: {ex.Message}", ex);
+            }
+        }
 
-			return _mapper.Map<AppUserRoleModelView>(existingUserRole);
-		}
+        public async Task<string> UpdateAppUserRoleAsync(string UserId, string RoleId, UpdateAppUserRoleModelView model)
+        {
+            try
+            {
+                // Validate UserId and RoleId
+                if (string.IsNullOrWhiteSpace(UserId) && string.IsNullOrWhiteSpace(RoleId))
+                {
+                    throw new Exception("Please provide a valid Application User Role ID.");
+                }
 
-		public async Task<AppUserRoleModelView> UpdateAppUserRoleAsync(string UserId, string RoleId, UpdateAppUserRoleModelView model)
-		{
-			if (string.IsNullOrWhiteSpace(UserId) && string.IsNullOrWhiteSpace(RoleId))
-			{
-				throw new Exception("Please provide a valid Application User Role ID.");
-			}
+                // get userRoles by id and not deleted
+                ApplicationUserRoles existingUserRole = await _unitOfWork.GetRepository<ApplicationUserRoles>().Entities
+                    .FirstOrDefaultAsync(s => s.UserId == Guid.Parse(UserId) && s.RoleId == Guid.Parse(RoleId) && !s.DeletedTime.HasValue)
+                    ?? throw new Exception("The Application User Role cannot be found or has been deleted!");
 
-			ApplicationUserRoles existingUserRole = await _unitOfWork.GetRepository<ApplicationUserRoles>().Entities
-				.FirstOrDefaultAsync(s => s.UserId == Guid.Parse(UserId) && s.RoleId == Guid.Parse(RoleId) && !s.DeletedTime.HasValue)
-				?? throw new Exception("The Application User Role cannot be found or has been deleted!");
+                // update userId if valid 
+                if (!string.IsNullOrWhiteSpace(model.UserId))
+                {
+                    existingUserRole.UserId = Guid.Parse(model.UserId);
+                }
 
-			_mapper.Map(model, existingUserRole);
+                // update roleId if valid
+                if (!string.IsNullOrWhiteSpace(model.RoleId))
+                {
+                    existingUserRole.RoleId = Guid.Parse(model.RoleId);
+                }
 
-			existingUserRole.LastUpdatedBy = "claim account";
-			existingUserRole.LastUpdatedTime = DateTimeOffset.UtcNow;
+                // update LastUpdatedBy, LastUpdatedTime
+                existingUserRole.LastUpdatedBy = _contextAccessor.HttpContext?.User?.FindFirst("userId")?.Value;
+                existingUserRole.LastUpdatedTime = DateTimeOffset.UtcNow;
 
-			_unitOfWork.GetRepository<ApplicationUserRoles>().Update(existingUserRole);
-			await _unitOfWork.SaveAsync();
+                // save changes
+                await _unitOfWork.GetRepository<ApplicationUserRoles>().UpdateAsync(existingUserRole);
+                await _unitOfWork.SaveAsync();
 
-			return _mapper.Map<AppUserRoleModelView>(existingUserRole);
-		}
-	}
+                return "update UserRole successfully";
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error occurred while updating user role: {ex.Message}", ex);
+            }
+        }
+    }
+
 }
