3e557a3f69c4353417dc56b9f48ba98b83a47ff8
DangKhoi <kwoitd03@gmail.com>
2024-11-06 15:53:36 +0700

    Feate: RazorPage SalaryPayment

---
+++ HairSalonBookingApp/HairSalon.RazorPage/Pages/SalaryPayments/Index.cshtml.cs
@@ -0,0 +1,44 @@
+using HairSalon.Contract.Services.Interface;
+using HairSalon.Core;
+using HairSalon.ModelViews.SalaryPaymentModelViews;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+public class SalaryManagentModel : PageModel
+{
+    private readonly ISalaryPaymentService _salaryService;
+
+    public SalaryManagentModel(ISalaryPaymentService salaryService)
+    {
+        _salaryService = salaryService;
+    }
+
+    public BasePaginatedList<SalaryPaymentModelView> SalaryPayments { get; set; }
+    public Dictionary<Guid, string> UserInfos { get; set; } = new Dictionary<Guid, string>();
+
+    public async Task<IActionResult> OnGetAsync(string? id, Guid? stylistId, DateTime? paymentDate, decimal? baseSalary, int pageNumber = 1, int pageSize = 5)
+    {
+        var userRolesJson = HttpContext.Session.GetString("UserRoles");
+
+        if (userRolesJson != null)
+        {
+            var userRoles = JsonConvert.DeserializeObject<List<string>>(userRolesJson);
+
+            if (!userRoles.Any(role => role == "Admin"))
+            {
+                TempData["ErrorMessage"] = "You do not have permission to view this page.";
+                return Page();
+            }
+        }
+        else
+        {
+            TempData["ErrorMessage"] = "You do not have permission to view this page.";
+            return Page();
+        }
+
+        SalaryPayments = await _salaryService.GetAllSalaryPaymentAsync(id, stylistId, paymentDate, baseSalary, pageNumber, pageSize);
+
+        return Page();
+    }
+}
