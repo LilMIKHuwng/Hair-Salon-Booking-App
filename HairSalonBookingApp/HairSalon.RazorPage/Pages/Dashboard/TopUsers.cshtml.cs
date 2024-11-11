dbd1e037eb46d7ed96ae648414d0553114944ee3
LilMIKHuwng <hunglqse171422@fpt.edu.vn>
2024-11-11 15:56:18 +0700

    Feat: Add Redis Cache Role

--- HairSalonBookingApp/HairSalon.RazorPage/Pages/Dashboard/TopUsers.cshtml.cs
+++ HairSalonBookingApp/HairSalon.RazorPage/Pages/Dashboard/TopUsers.cshtml.cs
@@ -2,6 +2,7 @@ using HairSalon.Contract.Services.Interface;
 using HairSalon.Core;
 using HairSalon.ModelViews.DashboardModelViews;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace HairSalon.RazorPage.Pages.Dashboard
 {
@@ -23,6 +24,22 @@ namespace HairSalon.RazorPage.Pages.Dashboard
 
 		public async Task OnGet(int? top, int pageNumber = 1, int pageSize = 5)
 		{
+			// Retrieve user roles from session
+			var userRolesJson = HttpContext.Session.GetString("UserRoles");
+			if (userRolesJson == null)
+			{
+				TempData["ErrorMessage"] = "You do not have permission to view this page.";
+				RedirectToPage("/Error");
+			}
+
+			var userRoles = JsonConvert.DeserializeObject<List<string>>(userRolesJson);
+
+			// Check if the user has "Admin" or "Manager" or "User" roles
+			if (!userRoles.Any(role => role == "Admin" || role == "Manager"))
+			{
+				TempData["DeniedMessage"] = "You do not have permission to view this page.";
+                RedirectToPage(); // Redirect to a different page with a denied message
+            }
 			// Store the value of 'top' in the property for UI use
 			Top = top;
 
