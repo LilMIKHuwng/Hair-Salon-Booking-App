6590ca7865027b2a2395a50ddb5395e2f19e902a
LilMIKHuwng <hunglqse171422@fpt.edu.vn>
2024-11-14 08:41:39 +0700

    Feat: Complete

--- HairSalonBookingApp/HairSalon.ModelViews/VnPayModelViews/PaymentResponseModelView.cs
+++ HairSalonBookingApp/HairSalon.ModelViews/VnPayModelViews/PaymentResponseModelView.cs
@@ -19,6 +19,5 @@ namespace HairSalon.ModelViews.VnPayModelViews
         public string? TransactionNo { get; set; }
         public string? TransactionStatus { get; set; }
         public string Method { get; set; }
-        public Guid? UserId { get; set; }
     }
 }
